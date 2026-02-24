a48b30c29aac883f248c1b261054266639a22d13
ABBAS ALMUKHTAFI <abbasyemen2@gmail.com>
2026-02-24 10:10:54 +0000

    Added all 6 question categories and seed system

--- models/Question.js
+++ models/Question.js
@@ -1,12 +1,31 @@
 const mongoose = require("mongoose");
 
 const questionSchema = new mongoose.Schema({
-  category: String, // AI, Planets, etc
-  difficulty: String, // easy, medium, hard
-  question: String,
-  options: [String],
-  correctAnswer: String,
-  points: Number
-});
+  category: {
+    type: String,
+    required: true
+  },
+  difficulty: {
+    type: String,
+    enum: ["easy", "medium", "hard"],
+    required: true
+  },
+  question: {
+    type: String,
+    required: true
+  },
+  options: {
+    type: [String],
+    required: true
+  },
+  correctAnswer: {
+    type: String,
+    required: true
+  },
+  points: {
+    type: Number,
+    required: true
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Question", questionSchema);
